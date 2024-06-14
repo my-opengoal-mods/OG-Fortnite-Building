6e905d32e3627c2af290d60067b6859200d089ce
Zedb0T <89345505+Zedb0T@users.noreply.github.com>
2024-06-14 19:04:33 -0400

    comment out v2 build actor just leave test-actor

--- custom_assets/jak1/levels/test-zone/testzone.gd
+++ custom_assets/jak1/levels/test-zone/testzone.gd
@@ -5,9 +5,9 @@
 ("TSZ.DGO"
  ("static-screen.o"
   "test-zone-obs.o"
-  "jd_ramp.o"
+;;   "jd_ramp.o"
   "plat-ag.go"
   "test-actor-ag.go"
-  "jd_ramp-ag.go"
+;;   "jd_ramp-ag.go"
   "test-zone.go"
  ))
